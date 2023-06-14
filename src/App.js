5a368f12dc71d905aa0450c3281d8b995327bf29
WASIM SOOMRO <soomrowasimw@gmail.com>
2023-06-13 21:34:33 -0600

    Netlify Error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import axios from 'axios';
 
 
